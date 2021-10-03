ba520e5744fff362922b82bad1ac8af07457449f
Jinhee <smswms1112@gmail.com>
2021-10-03 15:32:25 +0900

    회원가입부분에도 하단 특허정보 넣음

--- membership/membership4.php
+++ membership/membership4.php
@@ -10,6 +10,13 @@ if (get_device() == "P") {
 ?>
 <style type="css/text">
     .hidden_class{display:none}
+
+    .company__content{
+        padding-top: 20px;
+    }
+    .company__content ol li a {
+        text-decoration: underline;
+    }
 	
 </style>
 <section id="container">
@@ -33,6 +40,22 @@ if (get_device() == "P") {
                     <b class="on_btn">
                         <a href="../main/main.php">취소</a>
                     </b>
+                </div>
+                <div class="company__content mt-5" style="padding-top: 20px; padding-left:10%">
+                    <ol class="">
+                        <li class="" style="padding-bottom:10px">
+                            . 하단 사업자번호로 사업자상태 확인하세요. <a href="https://teht.hometax.go.kr/websquare/websquare.html?w2xPath=/ui/ab/a/a/UTEABAAA13.xml" class="" target="_blank">(국세청사이트)</a>
+                        </li>
+                        <li class="" style="padding-bottom:10px">
+                            . 통신판매업정보 확인하세요. <a href="http://www.ftc.go.kr/bizCommPop.do?wrkr_no=7450601064" class="" target="_blank">(공정거래위원회사이트)</a>
+                        </li>
+                        <li class="" style="padding-bottom:10px">
+                            . 리스맨 상표권 확인하세요.(출원번호 4020170015461(출원일 2017.02.07)) <a href="http://www.kipris.or.kr/khome/main.jsp" class="" target="_blank">(특허정보넷 키프리스)</a>
+                        </li>
+                        <li class="" style="padding-bottom:10px">
+                            . 리스맨 기술특허 확인하세요 (임대사업자 관리 장치 및 그 방법(APPARATUS AND THE METHOD FOR MANAGING RENTAL OR LEASE), 특허등록번호 10-1878261-0000). <a href="http://www.kipris.or.kr/khome/main.jsp" class="" target="_blank">(특허정보넷 키프리스)</a>
+                        </li>
+                    </ol>
                 </div>
             </form>
         </div>
